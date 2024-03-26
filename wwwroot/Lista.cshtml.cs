e0b94c73e6d5cac1b2a17deedc695582e038bd1f
User <User@DESKTOP-6RCTC8V>
2024-03-26 16:01:29 -0300

    Adicionar arquivos de projeto.

---
+++ wwwroot/Lista.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pexinxa.wwwroot
+{
+    public class ListaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/Mercado.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pexinxa.wwwroot
+{
+    public class MercadoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
